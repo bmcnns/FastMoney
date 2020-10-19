2ce9cf837ef6afbfe5c86423b1ff19b3837983d6
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2020-10-18 23:54:27 -0300

    implementation: Adds  ability to register and login.

--- app/src/main/java/ca/dal/cs/csci3130/project/MainActivity.java
+++ app/src/main/java/ca/dal/cs/csci3130/project/MainActivity.java
@@ -3,12 +3,100 @@ package ca.dal.cs.csci3130.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void submitForm(View view) {
+        TextInputEditText usernameField = (TextInputEditText)this.findViewById(R.id.username_field);
+        TextInputEditText emailField = (TextInputEditText)this.findViewById(R.id.email_field);
+        final String username = usernameField.getText().toString();
+        String email = emailField.getText().toString();
+
+        RegistrationValidator validator = new RegistrationValidator();
+
+        if (!validator.isValidUsername(username)) {
+            TextView usernameError = (TextView)this.findViewById(R.id.username_error);
+            usernameError.setVisibility(View.VISIBLE);
+
+            if (validator.hasNonAlphanumericCharacters(username))
+            {
+                usernameError.setText(R.string.register_username_alphanumeric_error);
+            }
+            else if (validator.isMissingValue(username))
+            {
+                usernameError.setText(R.string.register_username_missing_error);
+            }
+            else {
+                usernameError.setText(R.string.unknown_error);
+            }
+        }
+
+        if (!validator.isValidEmail(email)) {
+            TextView emailError = (TextView)this.findViewById(R.id.email_error);
+            emailError.setVisibility(View.VISIBLE);
+
+            if (validator.isMissingValue(email))
+            {
+                emailError.setText(R.string.register_email_missing_error);
+            }
+            else if (!validator.isValidEmailFormat(email))
+            {
+                emailError.setText(R.string.register_email_format_error);
+            }
+            else {
+                emailError.setText(R.string.unknown_error);
+            }
+        }
+
+        if (validator.isValidUsername(username) && validator.isValidEmail(email)) {
+
+            final User user = new User(username, email);
+            final String givenUsername = username;
+            final String givenEmail = email;
+
+            // Create the pop-up
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            firebaseAuth.signInWithEmailAndPassword(email, username)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                TextView existingAccountError = findViewById(R.id.existingAccountError);
+                                existingAccountError.setVisibility(View.VISIBLE);
+                                firebaseAuth.signOut();
+                            } else {
+                                firebaseAuth.createUserWithEmailAndPassword(givenEmail, givenUsername);
+
+                                builder.setTitle("Welcome " + user.getUsername() + "!")
+                                        .setMessage("A welcome email was sent to " + user.getEmail())
+                                        .show();
+                            }
+                        }
+                    });
+        }
     }
 }
--- app/src/main/java/ca/dal/cs/csci3130/project/RegistrationValidator.java
+++ app/src/main/java/ca/dal/cs/csci3130/project/RegistrationValidator.java
@@ -1,11 +1,37 @@
 package ca.dal.cs.csci3130.project;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegistrationValidator {
-    public boolean isValidUsername(String s) {
-        return false;
+    public boolean hasNonAlphanumericCharacters(String text)
+    {
+        Pattern nonAlphanumericCharacters = Pattern.compile("[^a-zA-Z0-9]");
+        Matcher matcher = nonAlphanumericCharacters.matcher(text);
+        return matcher.find();
     }
 
-    public boolean isValidEmail(String s) {
-        return false;
+    public boolean isMissingValue(String text)
+    {
+        return text.isEmpty();
+    }
+    public boolean isValidUsername(String username)
+    {
+        return !hasNonAlphanumericCharacters(username) && !isMissingValue(username);
+    }
+
+    public boolean isValidEmailFormat(String text)
+    {
+        Pattern validEmail = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
+        Matcher matcher = validEmail.matcher(text);
+        return matcher.matches();
+    }
+
+    public boolean isValidEmail(String email) {
+        return isValidEmailFormat(email) && !isMissingValue(email);
+    }
+
+    public boolean isValidUser(User user) {
+        return isValidEmail(user.getEmail()) && isValidUsername(user.getUsername());
     }
 }
