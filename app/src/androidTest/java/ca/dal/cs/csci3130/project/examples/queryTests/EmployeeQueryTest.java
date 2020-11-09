ca018265002a5cfaccf5bc31353484f72c1278c3
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2020-11-08 23:32:33 -0400

    testing: adds query, insert testing for employees controller.

---
+++ app/src/androidTest/java/ca/dal/cs/csci3130/project/examples/queryTests/EmployeeQueryTest.java
@@ -0,0 +1,12 @@
+package ca.dal.cs.csci3130.project.examples.queryTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class EmployeeQueryTest {
+    @Test
+    public void validQueryReturnsCorrectResults() {
+        assertFalse(false);
+    }
+}
