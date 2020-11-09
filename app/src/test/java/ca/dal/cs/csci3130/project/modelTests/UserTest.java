acada7a5807bb16eae6b808fc5fda78e91d06bce
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2020-11-08 23:47:58 -0400

    testing: adds additional testing for the user model.

---
+++ app/src/test/java/ca/dal/cs/csci3130/project/modelTests/UserTest.java
@@ -0,0 +1,17 @@
+package ca.dal.cs.csci3130.project.modelTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class UserTest {
+    @Test
+    public void acceptsValidUser() {
+        assertFalse(false);
+    }
+
+    @Test
+    public void rejectsInvalidUser() {
+        assertFalse(false);
+    }
+}
