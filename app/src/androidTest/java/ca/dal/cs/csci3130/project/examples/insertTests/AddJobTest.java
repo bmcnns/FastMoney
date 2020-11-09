0aad33eb33a3a3a1cdbbac10863c3e801d914bc2
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2020-11-08 23:37:59 -0400

    testing: adds query, inert testing for jobs controller.

---
+++ app/src/androidTest/java/ca/dal/cs/csci3130/project/examples/insertTests/AddJobTest.java
@@ -0,0 +1,12 @@
+package ca.dal.cs.csci3130.project.examples.insertTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class AddJobTest {
+    @Test
+    public void jobSuccessfullyAddedToDatabase() {
+        assertFalse(false);
+    }
+}
