03c1368cf9c1028958e1b6c5fe04aaa56a9e5d46
SteveChen <zzchenyufei@outlook.com>
2020-11-27 13:07:58 -0400

    - Adding meterial deisgn for registration page

--- app/src/main/java/ca/dal/cs/csci3130/project/views/RegistrationActivity.java
+++ app/src/main/java/ca/dal/cs/csci3130/project/views/RegistrationActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -31,7 +32,7 @@ public class RegistrationActivity extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     Button registerBtn;
-//    TextView loginBtn;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +41,14 @@ public class RegistrationActivity extends AppCompatActivity {
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         registerBtn = findViewById(R.id.registerRGBtn);
+        loginBtn = findViewById(R.id.loginRGBtn);
         final TextInputEditText emailField = this.findViewById(R.id.email_field);
         final TextInputEditText passwordField = this.findViewById(R.id.password_field);
-        final EditText firstNameField = this.findViewById(R.id.first_name_field);
-        final EditText lastNameField = this.findViewById(R.id.last_name_field);
-        final EditText creditCardNumField = this.findViewById(R.id.credit_card_Num_field);
-        final EditText creditCardExpireField = this.findViewById(R.id.credit_card_expire_field);
-        final EditText cvvField = this.findViewById(R.id.cvv_field);
+        final TextInputEditText firstNameField = this.findViewById(R.id.first_name_field);
+        final TextInputEditText lastNameField = this.findViewById(R.id.last_name_field);
+//        final EditText creditCardNumField = this.findViewById(R.id.credit_card_Num_field);
+//        final EditText creditCardExpireField = this.findViewById(R.id.credit_card_expire_field);
+//        final EditText cvvField = this.findViewById(R.id.cvv_field);
 
 
         /**
@@ -60,9 +62,9 @@ public class RegistrationActivity extends AppCompatActivity {
                 final String password = passwordField.getText().toString();
                 final String firstName = firstNameField.getText().toString();
                 final String lastName = lastNameField.getText().toString();
-                final String creditCardNum = creditCardNumField.getText().toString();
-                final String creditCardExpire = creditCardExpireField.getText().toString();
-                final String ccv = cvvField.getText().toString();
+//                final String creditCardNum = creditCardNumField.getText().toString();
+//                final String creditCardExpire = creditCardExpireField.getText().toString();
+//                final String ccv = cvvField.getText().toString();
 
                 //if user is already logged in
                 if (fAuth.getCurrentUser() != null) {
@@ -84,9 +86,9 @@ public class RegistrationActivity extends AppCompatActivity {
                                     user.put("password", password);
                                     user.put("firstName", firstName);
                                     user.put("lastName", lastName);
-                                    user.put("creditCardNum", creditCardNum);
-                                    user.put("creditCardExpire", creditCardExpire);
-                                    user.put("ccv", ccv);
+                                    user.put("creditCardNum", 1);
+                                    user.put("creditCardExpire", 1);
+                                    user.put("ccv", 1);
 
 
                                     /** Store extra User data to firestone once successful registration to firebase authentication
@@ -96,7 +98,7 @@ public class RegistrationActivity extends AppCompatActivity {
                                         @Override
                                         public void onSuccess(Void aVoid) {
                                             Log.d("", "user have been registered");
-                                            Toast.makeText(RegistrationActivity.this, "user have been registered", Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(RegistrationActivity.this, "Successfully registered", Toast.LENGTH_SHORT).show();
                                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                                         }
 
@@ -116,6 +118,15 @@ public class RegistrationActivity extends AppCompatActivity {
             }
 
 
+        });
+
+
+        /* Login button to the login Page*/
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
         });
     }
 }
