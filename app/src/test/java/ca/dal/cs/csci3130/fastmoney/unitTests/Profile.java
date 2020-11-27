26d7fc7c4dcdbb6de2bf8d2f7187ba48b50435d4
Bryce MacInnis <bryce.macinnis@dal.ca>
2020-11-26 23:49:26 -0400

    Adds unit test stubs

---
+++ app/src/test/java/ca/dal/cs/csci3130/fastmoney/unitTests/Profile.java
@@ -0,0 +1,32 @@
+package ca.dal.cs.csci3130.fastmoney.unitTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Profile {
+    @Test
+    public void profileTest() {
+        assertFalse(!false);
+    }
+
+    @Test
+    public void employerRatingTest() {
+        assertFalse(!false);
+    }
+
+    @Test
+    public void employeeRatingTest() {
+        assertFalse(!false);
+    }
+
+    @Test
+    public void nameTest() {
+        assertFalse(!false);
+    }
+
+    @Test
+    public void creditCardTest() {
+        assertFalse(!false);
+    }
+}
---
+++ app/src/test/java/ca/dal/cs/csci3130/fastmoney/unitTests/Rating.java
@@ -0,0 +1,12 @@
+package ca.dal.cs.csci3130.fastmoney.unitTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Rating {
+    @Test
+    public void ratingTest() {
+        assertFalse(!false);
+    }
+}
