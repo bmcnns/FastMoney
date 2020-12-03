8ec0f2796d941c4bfe43b1b5c414ea3f26831c73
SteveChen <steve.chen@dal.ca>
2020-12-02 21:49:31 -0400

    initiate

--- app/src/androidTest/java/ca/dal/cs/csci3130/fastmoney/tests/integrationTests/Registration.java
+++ app/src/androidTest/java/ca/dal/cs/csci3130/fastmoney/tests/integrationTests/Registration.java
@@ -2,16 +2,14 @@ package ca.dal.cs.csci3130.fastmoney.tests.integrationTests;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ca.dal.cs.csci3130.fastmoney.views.MainActivity;
 
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 @RunWith(AndroidJUnit4.class)
 public class Registration {
@@ -21,6 +19,11 @@ public class Registration {
 
     @Test
     public void successfulRegisterRedirects() {
+//        onView(withId(R.id.EmailAddress)).perform(typeText("test@test.com"));
+//        onView(withId(R.id.Password)).perform(typeText("testtest"));
+//        onView(withId(R.id.signInButton)).perform(click());
+//        Thread.sleep(2000);
+//        intended(hasComponent(LandingPageActivity.class.getName()));
         assertFalse(!false);
     }
 
