14fc5851e39883375f34995fb74272f513acf1bd
SteveChen <steve.chen@dal.ca>
2020-12-05 19:10:20 -0400

    Adding and finish activity_rating.xml design

---
+++ app/src/main/java/ca/dal/cs/csci3130/fastmoney/views/RatingActivity.java
@@ -0,0 +1,16 @@
+package ca.dal.cs.csci3130.fastmoney.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.dal.cs.csci3130.fastmoney.R;
+
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+    }
+}
