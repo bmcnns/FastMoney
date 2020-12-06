51580144efb0a22c8332a773587785bf0f0223fd
SteveChen <steve.chen@dal.ca>
2020-12-06 15:40:36 -0400

    Fix bug - id: 6

--- app/src/main/java/ca/dal/cs/csci3130/fastmoney/views/LandingPageActivity.java
+++ app/src/main/java/ca/dal/cs/csci3130/fastmoney/views/LandingPageActivity.java
@@ -1,33 +1,72 @@
 package ca.dal.cs.csci3130.fastmoney.views;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import java.util.Map;
 
 import ca.dal.cs.csci3130.fastmoney.R;
 
 public class LandingPageActivity extends AppCompatActivity {
 
-    FirebaseAuth fAuth;
+    String fName, lName;
 
+
+    FirebaseAuth fAuth=FirebaseAuth.getInstance();
+    FirebaseFirestore db= FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
 
-        fAuth = FirebaseAuth.getInstance();
+        final TextView welcomeMessage = (TextView)findViewById(R.id.welcomeHeader);
+        final TextView signOutLink = (TextView)findViewById(R.id.signOutLink);
 
         if (fAuth.getCurrentUser() == null) {
             startActivity(new Intent(getApplicationContext(), LogInActivity.class));
             finish();
         }
+
+        db.collection("users").document((String)fAuth.getCurrentUser().getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            private static final String TAG = "db";
+
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    if(document.exists()){
+                        Map<String, Object> data = document.getData();
+                        fName = (String)data.get("firstName");
+                        lName = (String)data.get("lastName");
+                        welcomeMessage.setText("Welcome, "+fName);
+                        signOutLink.setText("Not "+fName+"? Sign out.");
+                    }else {
+                        Log.w(TAG, "Error getting documents.", task.getException());
+                    }
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.getException());
+                }
+            }
+        });
+
+
     }
 
     public void signOut(View view) {
