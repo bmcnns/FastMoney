b0824ba693a6b566fcfbb6e2478a355a575e75fb
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2020-12-07 03:00:57 -0400

    testing: adds espresso tests for HireActivity

--- app/src/androidTest/java/ca/dal/cs/csci3130/fastmoney/tests/espressoTests/Hire.java
+++ app/src/androidTest/java/ca/dal/cs/csci3130/fastmoney/tests/espressoTests/Hire.java
@@ -4,13 +4,21 @@ import android.content.Context;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import ca.dal.cs.csci3130.fastmoney.views.HireActivity;
 import ca.dal.cs.csci3130.fastmoney.views.MainActivity;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
@@ -19,42 +27,58 @@ import static org.junit.Assert.*;
 public class Hire {
 
     @Rule
-    public ActivityScenarioRule<MainActivity> myRule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<HireActivity> myRule = new ActivityScenarioRule<>(HireActivity.class);
 
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ca.dal.cs.csci3130.fastmoney.examples.espressoTests", appContext.getPackageName());
+    @Before
+    public void setUp() {
+        HireActivity.TEST_MODE = "";
     }
 
     @Test
     public void showsFilledJobs() {
-        assertFalse(!false);
-    }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-    @Test
-    public void showsCorrectAmountOfFilledJobs() {
-        assertFalse(!false);
+        onView(withText("Your Filled Jobs")).perform(scrollTo());
+        onView(withText("Your Filled Jobs")).check(matches(isDisplayed()));
     }
 
     @Test
     public void showsUnfilledJobs() {
-        assertFalse(!false);
-    }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-    @Test
-    public void showsCorrectAmountOfUnfilledJobs() {
-        assertFalse(!false);
+        onView(withText("Your Unfilled Jobs")).perform(scrollTo());
+        onView(withText("Your Unfilled Jobs")).check(matches(isDisplayed()));
     }
 
     @Test
     public void showsNoJobsWhenNoJobs() {
-        assertFalse(!false);
+        HireActivity.TEST_MODE = "NO_JOBS";
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText("You haven't posted any jobs.")).check(matches(isDisplayed()));
     }
 
     @Test
     public void showsPostAJobButtonWhenNoJobs() {
-        assertFalse(!false);
+        HireActivity.TEST_MODE = "NO_JOBS";
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("Post Job")).check(matches(isDisplayed()));
     }
 }
