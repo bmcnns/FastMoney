c4f5acdf47ce2f6452b351de708c382a7b64d7c9
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2020-12-07 05:25:32 -0400

    testing: makes espresso tests for LogIn activity pass.

--- app/src/androidTest/java/ca/dal/cs/csci3130/fastmoney/tests/espressoTests/LogIn.java
+++ app/src/androidTest/java/ca/dal/cs/csci3130/fastmoney/tests/espressoTests/LogIn.java
@@ -3,6 +3,9 @@ package ca.dal.cs.csci3130.fastmoney.tests.espressoTests;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +15,8 @@ import ca.dal.cs.csci3130.fastmoney.views.LogInActivity;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -21,6 +26,13 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class LogIn {
+
+    @Before
+    public void setUp() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+    }
+
     @Rule
     public ActivityScenarioRule<LogInActivity> myRule = new ActivityScenarioRule<>(LogInActivity.class);
 
@@ -61,9 +73,9 @@ public class LogIn {
     //ensure proper handling of invalid sign in attempts
     @Test
     public void invalidUserSignIn(){
-        onView(withId(R.id.EmailAddress)).perform(typeText("this-is-not-a-valid-email"));
-        onView(withId(R.id.Password)).perform(typeText("~!@#$%^&*"));
-        onView(withId(R.id.signInButton)).perform(click());
+        onView(withId(R.id.EmailAddress)).perform(replaceText("this-is-not-a-valid-email"), closeSoftKeyboard());
+        onView(withId(R.id.Password)).perform(replaceText("~!@#$%^&*"), closeSoftKeyboard());
+        onView(withText("Sign In")).perform(closeSoftKeyboard(), click());
         onView(withText("Invalid email or password")).check(matches(isDisplayed()));
     }
 
