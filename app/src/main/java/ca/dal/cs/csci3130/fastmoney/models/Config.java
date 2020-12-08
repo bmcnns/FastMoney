8600e6c2d63645241a47169915126ec321ffb04a
SteveChen <steve.chen@dal.ca>
2020-12-08 11:16:30 -0400

    add payment

---
+++ app/src/main/java/ca/dal/cs/csci3130/fastmoney/models/Config.java
@@ -0,0 +1,5 @@
+package ca.dal.cs.csci3130.fastmoney.models;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AaC8X8Q9gpnMJ7cnIpUTU-etIh6H6ARfsjI9T4_h9g1pU1lrI1ZNJsiRquPKoYBblRuCps6IJeH3pAVD";
+}
--- app/src/main/java/ca/dal/cs/csci3130/fastmoney/views/RatingActivity.java
+++ app/src/main/java/ca/dal/cs/csci3130/fastmoney/views/RatingActivity.java
@@ -1,28 +1,48 @@
 package ca.dal.cs.csci3130.fastmoney.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
-import android.media.Image;
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 import ca.dal.cs.csci3130.fastmoney.R;
+import ca.dal.cs.csci3130.fastmoney.models.Config;
+import ca.dal.cs.csci3130.fastmoney.models.PaymentStatus;
 
 public class RatingActivity extends AppCompatActivity {
+
+    private static final int PAYPAL_REQUEST_CODE = 555;
+    int amount = 10;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId(Config.PAYPAL_CLIENT_ID);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+        startService(intent);
     }
 
     private int extractRating() {
@@ -105,6 +125,45 @@ public class RatingActivity extends AppCompatActivity {
         else {
             showErrorMessage();
         }
+    }
+
+    public void pay(View v){
+
+        PayPalPayment payPalPayment = new PayPalPayment(new BigDecimal(String.valueOf(amount)),"CAD",
+                "Purchase Service",PayPalPayment.PAYMENT_INTENT_SALE);
+        Intent intent = new Intent(this, PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
+        startActivityForResult(intent,PAYPAL_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirmation != null) {
+                    try {
+                        String paymentDetails = confirmation.toJSONObject().toString();
+                        Log.d("Details",paymentDetails);
+                        startActivity(new Intent(this, PaymentStatus.class)
+                                .putExtra("PaymentDetails",paymentDetails)
+                                .putExtra("Amount",amount));
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED)
+                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+        } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID)
+            Toast.makeText(this, "Invalid", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(this,PayPalService.class));
+        super.onDestroy();
     }
 
 }
